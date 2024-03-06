54c857f9dcbe886a11f9130a875861ca54053f50
ritikj27 <ritik.j@preciousinfosystem.com>
2024-03-06 17:12:55 +0530

    learn my objects

--- 02basic/functions.ts
+++ 02basic/functions.ts
@@ -1,21 +1,21 @@
-function addTwo(num: number): number{
-    return num + 2
-   //return "hello"
+function addTwo(num: number): number {
+  return num + 2;
+  //return "hello"
 }
 
-function getUpper(val: string){
-   return val.toUpperCase()
+function getUpper(val: string) {
+  return val.toUpperCase();
 }
 
-function signUpUser(name: string, email: string, isPaid: boolean){}
+function signUpUser(name: string, email: string, isPaid: boolean) {}
 
-let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
-let myValue = addTwo(5)
-getUpper("hitesh")
+let myValue = addTwo(5);
+getUpper("hitesh");
 
-signUpUser("hitesh", "hitesh@lco.dev", false)
-loginUser("h", "h@h.com")
+signUpUser("hitesh", "hitesh@lco.dev", false);
+loginUser("h", "h@h.com");
 
 // function getValue(myVal: number): boolean{
 //     if (myVal > 5) {
@@ -24,27 +24,22 @@ loginUser("h", "h@h.com")
 //     return "200 OK"
 // }
 
-const getHello = (s: string):string => {
-   return ""
-}
+const getHello = (s: string): string => {
+  return "";
+};
 
-const heros = ["thor", "spiderman", "ironman"]
+const heros = ["thor", "spiderman", "ironman"];
 // const heros = [1, 2, 3]
 
 heros.map((hero): string => {
-   return `hero is ${hero}`
-})
+  return `hero is ${hero}`;
+});
 
-
-function consoleError(errmsg: string): void{
-   console.log(errmsg);
-
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
 }
-function handleError(errmsg: string): never{
-   throw new Error(errmsg);
-
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
 }
 
-
-
-export {}
+export {};
---
+++ 02basic/myObjects.ts
@@ -0,0 +1,31 @@
+const User = {
+    name: "hitesh",
+    email: "hitesh@lco.dev",
+    isAvtive: true
+}
+
+function createUser({name: string, isPaid: boolean}){}
+
+let newUser = {name: "hitesh", isPaid: false, email: "h@h.com"}
+
+createUser(newUser)
+
+
+
+function createCourse():{name: string, price: number}{
+    return {name: "reactjs", price: 399}
+}
+
+
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive: boolean
+// }
+
+
+// function createUser(user: User): User{
+//     return {name: "", email: "", isActive: true}
+// }
+
+// createUser({name: "", email: "", isActive: true})
