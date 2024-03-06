2e217ef6682c43613501ae109e5ffd2397c4128d
ritikj27 <ritik.j@preciousinfosystem.com>
2024-03-06 16:22:40 +0530

    my functions

--- 02basic/functions.js
+++ 02basic/functions.js
@@ -1,20 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
-    return num;
+    return num + 2;
+    //return "hello"
 }
 function getUpper(val) {
     return val.toUpperCase();
 }
-function signupUser(name, email, isPaid) {
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
+};
+var myValue = addTwo(5);
+getUpper("hitesh");
+signUpUser("hitesh", "hitesh@lco.dev", false);
+loginUser("h", "h@h.com");
+// function getValue(myVal: number): boolean{
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3]
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
 }
-function getValue(myVal) {
-    if (myVal > 5) {
-        return true;
-    }
-    return "200 ok";
+function handleError(errmsg) {
+    throw new Error(errmsg);
 }
-var word = addTwo(7);
-getUpper("hello");
-signupUser("ritik", 'ritik@gmail.com', true);
