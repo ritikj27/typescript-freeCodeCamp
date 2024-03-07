2f61307459ad78c2362e2d294316dd6330eb0217
ritikj27 <ritik.j@preciousinfosystem.com>
2024-03-07 16:09:10 +0530

    abstract class in typescript

---
+++ 03purets/dist/abstractClass.js
@@ -0,0 +1,25 @@
+"use strict";
+// with abstract keyword we cannot able to create instance of object .
+class TakePhoto {
+    constructor(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+    getReelTime() {
+        //some complex calculation
+        return 8;
+    }
+}
+class youtube extends TakePhoto {
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSepia() {
+        console.log("Sepia");
+    }
+}
+const hc = new youtube("test", "Test", 3);
+hc.getReelTime();
---
+++ 03purets/src/abstractClass.ts
@@ -0,0 +1,27 @@
+// with abstract keyword we cannot able to create instance of object .
+
+abstract class TakePhoto {
+  constructor(public cameraMode: string, public filter: string) {}
+  abstract getSepia(): void;
+  getReelTime(): number {
+    //some complex calculation
+    return 8;
+  }
+}
+
+class youtube extends TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {
+    super(cameraMode, filter);
+  }
+  getSepia(): void {
+    console.log("Sepia");
+  }
+}
+
+const hc = new youtube("test", "Test", 3)
+
+hc.getReelTime()
