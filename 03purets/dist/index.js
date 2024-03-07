9fe184c31806b111870aad1b4756a144490f74df
ritikj27 <ritik.j@preciousinfosystem.com>
2024-03-07 13:35:31 +0530

    learn about classes in typescript

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
 console.log("hello");
+class User {
+    constructor(name, email) {
+        this.city = 'indore';
+        this.name = name,
+            this.email = email;
+    }
+}
+const user = new User('h', '@gmail.com');
+user.city = 'ri';
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,1 +1,13 @@
 console.log("hello")
+class User{
+    name:string
+    email:string
+    city:string = 'indore'
+    constructor(name:string,email:string){
+        this.name = name,
+        this.email = email;
+    }
+}
+
+const user = new User('h','@gmail.com')
+user.city='ri'
